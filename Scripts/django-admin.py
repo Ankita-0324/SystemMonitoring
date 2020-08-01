b911553d3769e97aaa64664bc116cb39b7578772
Ankita_0324 <ankitadeshmane312@gmail.com>
2020-08-01 23:55:34 +0530

    First Commit

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\akash\desktop\system_controlling\sensor_stm\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
